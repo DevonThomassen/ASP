ef7444c67c84afe33a3c4a94ac99ce162477fe5c
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-06-13 23:41:33 +0200

    Update add.aspx.cs

--- Lit/add.aspx.cs
+++ Lit/add.aspx.cs
@@ -10,4 +10,23 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e) {
 
     }
+    
+    protected void submitDelete_Click(object sender, EventArgs e) {
+        string connectie;
+        connectie = @"Data Source=DEVONTHOMASSEN\SQLEXPRESS;";
+        connectie += "Initial Catalog=homework; Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectie);
+        SqlCommand cmd = new SqlCommand("DELETE FROM subject WHERE name = '" + delete.SelectedValue + "'", conn);
+        SqlCommand cmd2 = new SqlCommand("DELETE FROM results WHERE subjectName = '" + delete.SelectedValue + "'", conn);
+        conn.Open();
+        try {
+            cmd.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            delete.DataBind();
+        }
+        catch {
+
+        }
+        conn.Close();
+    }
 }
